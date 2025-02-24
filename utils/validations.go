5eda8a4a37a98290f72525fb45c7322a38065715
Patsagorn <patsagorn_y@outlook.com>
2025-02-24 20:40:36 +0700

    refactor(validations): remove unnecessary print statement from phone validation function

--- utils/validations.go
+++ utils/validations.go
@@ -1,13 +1,11 @@
 package utils
 
 import (
-	"fmt"
 	"regexp"
 )
 
 // IsValidPhone validates a phone number using a regular expression.
 func IsValidPhone(phone string) bool {
-	fmt.Println(phone)
 	re := regexp.MustCompile(`^((\+66|0)(\d{1,2}\d{3}\d{3,4}))$`)
 	return re.MatchString(phone)
 }
