583124fedd8ca9f6bff52b444c6586ae0c1c7d31
Patsagorn <patsagorn_y@outlook.com>
2025-02-25 19:07:51 +0700

    feat(user): update SelectedSources field to use pq.StringArray type

--- domain/user.go
+++ domain/user.go
@@ -1,6 +1,10 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
 
 type Role string
 
@@ -11,23 +15,23 @@ const (
 )
 
 type User struct {
-	ID              string     `json:"id" gorm:"primaryKey"`
-	UID             string     `json:"uid" gorm:"unique"`
-	Name            string     `json:"name"`
-	Role            Role       `json:"role"`
-	Email           *string    `json:"email"`
-	Phone           string     `json:"phone" gorm:"unique"` // Make phone unique
-	BirtDate        *time.Time `json:"birthDate"`
-	Status          string     `json:"status"`      // ม.ต้น, ม.ปลาย, ปวช., ปวส. etc.
-	OtherStatus     *string    `json:"otherStatus"` // other status
-	Province        string     `json:"province"`
-	School          string     `json:"school"`
-	SelectedSources []string   `json:"selectedSources" gorm:"type:text[]"`
-	OtherSource     *string    `json:"otherSource"`
-	FirstInterest   string     `json:"firstInterest"`
-	SecondInterest  string     `json:"secondInterest"`
-	ThirdInterest   string     `json:"thirdInterest"`
-	Objective       string     `json:"objective"`
-	RegisteredAt    *time.Time `json:"registerAt"`
-	LastEntered     *time.Time `json:"lastEntered"` // Timestamp for the last QR scan
+	ID              string         `json:"id" gorm:"primaryKey"`
+	UID             string         `json:"uid" gorm:"unique"`
+	Name            string         `json:"name"`
+	Role            Role           `json:"role"`
+	Email           *string        `json:"email"`
+	Phone           string         `json:"phone" gorm:"unique"` // Make phone unique
+	BirtDate        *time.Time     `json:"birthDate"`
+	Status          string         `json:"status"`      // ม.ต้น, ม.ปลาย, ปวช., ปวส. etc.
+	OtherStatus     *string        `json:"otherStatus"` // other status
+	Province        string         `json:"province"`
+	School          string         `json:"school"`
+	SelectedSources pq.StringArray `json:"selectedSources" gorm:"type:text[]"`
+	OtherSource     *string        `json:"otherSource"`
+	FirstInterest   string         `json:"firstInterest"`
+	SecondInterest  string         `json:"secondInterest"`
+	ThirdInterest   string         `json:"thirdInterest"`
+	Objective       string         `json:"objective"`
+	RegisteredAt    *time.Time     `json:"registerAt"`
+	LastEntered     *time.Time     `json:"lastEntered"` // Timestamp for the last QR scan
 }
